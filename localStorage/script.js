f609daee2a03ac66be503d5e30680abb9618608d
LoZizz <riccardozini2@gmail.com>
2024-12-09 13:07:00 +0100

    Update script.js

--- localStorage/script.js
+++ localStorage/script.js
@@ -1,1 +1,31 @@
+function ricevi(){
+     localStorage.getItem("utenti",utenti)
+}
+
+const utenti = []
+
+const table = document.getElementById("tabella")
+
+function matrice(nome, cognome, indirizzo, citta){
+    const newUtente = []
+    newUtente.push(nome)
+    newUtente.push(cognome)
+    newUtente.push(indirizzo)
+    newUtente.push(citta)
+    // nuova riga
+    utenti.push(newUtente)
+}
+
+function utente(){
+    matrice(document.getElementById("nome").value, document.getElementById("cognome").value, document.getElementById("indirizzo").value, document.getElementById("citta").value)
+    let user = utenti[utenti.length-1]
+    // crea riga tabella
+    const tableRow = document.createElement("tr")
+    for(let i = 0; i<user.length; i++){
+        const tableD = document.createElement("td")
+        tableD.innerHTML = user[i]
+        tableRow.appendChild(tableD)
+    }
+    table.appendChild(tableRow)
+}
 

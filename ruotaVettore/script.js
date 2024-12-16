24b8f4363b1bb8074dcbd7079f2ed656eb1b9a31
LoZizz <riccardozini2@gmail.com>
2024-12-16 13:38:48 +0100

    Update script.js

--- ruotaVettore/script.js
+++ ruotaVettore/script.js
@@ -7,18 +7,23 @@ function gira(){
         vettore[i] = vettore[vettore.length-i-1]
         vettore[vettore.length-i -1] = temp
     }
-    console.log(vettore);
+    alert(vettore);
 }
 function ordina(){
-    let vett= document.getElementById("vettore").value;
-    let vettore = vett.split(" ");
-    for( i = 0; i< vettore.length){
-        for( j = 0; j<(vettore.length - i - 1) {
-            if( vettore[j] > vettore[j + 1] ){
-                let temp = vettore[i]
-                vettore[i] = vettore[vettore.length-i-1]
-                vettore[vettore.length-i -1] = tem
+    let vet= document.getElementById("vettore").value;
+    let vettore2 = vet.split(" ");
+    for(y=0; y<vettore2.length; y++){
+        vettore2[y] = parseInt(vettore2[y])
+    }
+    let temp
+    for( i = 0; i< vettore2.length; i++){
+        for( j = 0; j<(vettore2.length - 1); j++) {
+            if( vettore2[j] > vettore2[j + 1] ){
+                temp = vettore2[j]
+                vettore2[j] = vettore2[j+1]
+                vettore2[j+1] = temp
             }
         }
     }
+    alert(vettore2);
 }

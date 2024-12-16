9f11e7d347d053b03a5e6786b1116456877a574c
LoZizz <riccardozini2@gmail.com>
2024-12-16 12:28:57 +0100

    Update script.js

--- ruotaVettore/script.js
+++ ruotaVettore/script.js
@@ -9,3 +9,16 @@ function gira(){
     }
     console.log(vettore);
 }
+function ordina(){
+    let vett= document.getElementById("vettore").value;
+    let vettore = vett.split(" ");
+    for( i = 0; i< vettore.length){
+        for( j = 0; j<(vettore.length - i - 1) {
+            if( vettore[j] > vettore[j + 1] ){
+                let temp = vettore[i]
+                vettore[i] = vettore[vettore.length-i-1]
+                vettore[vettore.length-i -1] = tem
+            }
+        }
+    }
+}
